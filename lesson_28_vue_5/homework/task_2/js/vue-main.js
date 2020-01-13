5d3acaf3065673a58d9749a703e43dd61ca65b6c
Arsenii <Fedorov-Ars@yandex.ru>
2020-01-13 23:08:17 +0300

    vue_5 homework task-2 worked

--- lesson_28_vue_5/homework/task_2/js/vue-main.js
+++ lesson_28_vue_5/homework/task_2/js/vue-main.js
@@ -1,21 +1,52 @@
 Vue.component('carousel', {
     props: ['messages', 'timeout'],
-    // template: '<div><h2>Карусель:</h2><p>Name:{{ this.messages2 }}</p><p>Age: {{ timeout }}</p></div>',
-    // template: '<div><h2>Карусель:</h2><p>{{ messages }} {{ timeout }}</p></div>',
-    template: '<div><h2>Карусель:</h2><p>{{messages}} - {{ timeout }}</p></div>',
-    methods: {
-        startCarousel() {
-            let a = '<p>setInterval({{messages}},{{ timeout }})</p>'
+    template: '<div>' +
+        '<h2>Карусель:</h2>' +
+        '<button class="btn btn-success" @click="startTextSlider(messages, timeout)">Запустить карусель</button>' +
+        '<button class="btn btn-danger" @click="stopTextSlider">Остановить карусель</button>' +
+        '<p id="text-slider-message" class="border border-dark"></p>' +
+        '</div>',
+    data: function () {
+        return {
+            timerId: undefined,
+            currentMsg: 0,
         }
+    },
+    methods: {
+        startTextSlider(messages, timeout) {
+            function getCurrentMsg(messages) {
+                if ((messages.length === 1) && (messages[0] === '')) {
+                    alert('Заполните фразы');
+                } else {
+                    if (((++this.currentMsg) < (messages.length))) {
+                        $('#text-slider-message').text(messages[this.currentMsg]);
+                    } else {
+                        this.currentMsg = 0;
+                        $('#text-slider-message').text(messages[this.currentMsg]);
+                    }
+                }
+            }
 
+            this.timerId = setInterval(getCurrentMsg, timeout * 1000, messages);
+        },
+        stopTextSlider() {
+            if (this.timerId) {
+                clearInterval(this.timerId);
+            } else {
+                alert("Карусель еще не запущена!");
+            }
+        },
     },
 });
 
 let app = new Vue({
     el: '#app',
     data: {
-        // carousel: {messages: '', timeout: 5},
-        text: '',
+        text: 'Если тебе тяжело, значит ты поднимаешься в гору. Если тебе легко, значит ты летишь в пропасть.;' +
+            'Отпускайте идиотов и клоунов из своей жизни.' +
+            'Цирк должен гастролировать!;' +
+            'Глупца можно узнать по двум приметам: он много говорит о вещах, для него бесполезных, ' +
+            'и высказывается о том, про что его не спрашивают.',
         separator: ';',
         messages: [],
         timeout: 5,
