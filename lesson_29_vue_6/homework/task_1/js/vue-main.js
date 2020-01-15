f7b38cd8298b52a44ba110e38fcbe44066e5063d
Фёдоров Арсений Николаевич <Arseniy.Fedorov@russianpost.ru>
2020-01-15 18:23:15 +0300

    vue_6 homework task-1 ver.1

--- lesson_29_vue_6/homework/task_1/js/vue-main.js
+++ lesson_29_vue_6/homework/task_1/js/vue-main.js
@@ -1,4 +1,28 @@
+const Main = {
+    template: '<h1>Банер</h1>'
+};
+const NotFound = {
+    template: '<h2>Старницы отсутствет</h2>'
+};
+const Home = {
+    template: '<h2>Домашняя старница</h2>'
+};
+const About = {
+    template: '<h2>О нас</h2>'
+};
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/', component: Home},
+        {path: '/main', component: Main},
+        {path: '/about', component: About},
+        {path: '/', component: Home},
+    ]
+});
+
 let app = new Vue({
     el: '#app',
+    router: router,
     data: {}
 });
